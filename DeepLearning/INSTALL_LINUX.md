c3d690dabd7c2967a16e16a582cb039272467b2b
Holiday Pettijohn <hpettijohn20@vcschools.org>
2019-09-21 15:09:14 -0700

    Updated TensorFlow installer for Jetson Nano

---
+++ DeepLearning/INSTALL_LINUX.md
@@ -0,0 +1,4 @@
+These installation instructions are based off the documentation at https://devtalk.nvidia.com/default/topic/1048776/jetson-nano/official-tensorflow-for-jetson-nano-/
+They will only work for the Jetson Nano with JetPack 4.2S
+
+Run bash INSTALL_LINUX.sh
--- DeepLearning/INSTALL_LINUX.sh
+++ DeepLearning/INSTALL_LINUX.sh
@@ -1,23 +1,8 @@
 sudo apt update
-python get_model_zoo.py
-sudo apt install libatlas-base-dev libopenblas-dev libblas-dev hdf5-dev liblapack-dev gfortran
-sudo apt install python3-dev python3-pip
-sudo apt-get install protobuf-compiler python-pil python-lxml python-tk
-pip install Cython contextlib2 jupyter matplotlib keras
-git clone https://github.com/cocodataset/cocoapi.git
-git clone https://github.com/tensorflow/models.git
-rn cocoapi-master cocoapi
-rn models-master models
-py fix_cocoapi.py
-cd cocoapi/PythonAPI
-python setup.py build_ext --inplace
-cd ..
-cd ..
-cp -r cocoapi/PythonAPI/pycocotools tensorflow/models/research/
-cd tensorflow/ /research
-protoc object_detection/protos/*.proto --python_out=.
-python setup.py build
-python setup.py install
-wget -O tensorflow_gpu-py3-none-any.whl https://files.pythonhosted.org/packages/76/04/43153bfdfcf6c9a4c38ecdb971ca9a75b9a791bb69a764d652c359aca504/tensorflow_gpu-1.14.0-cp36-cp36m-manylinux1_x86_64.whl
-pip install tensorflow_gpu-py3-none-any.whl
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
+sudo apt-get install python3-pip
+pip3 install --user numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
+pip3 install --pre --user --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.14.0+nv19.7
+wget -O tensorflow_gpu-py3-none-any.whl https://developer.download.nvidia.com/compute/redist/jp/v42/tensorflow-gpu/tensorflow_gpu-1.14.0+nv19.7-cp36-cp36m-linux_aarch64.whl
+pip3 install tensorflow_gpu-py3-none-any.whl
 read -p "Installation successful. Press Enter to end..."
